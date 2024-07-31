2f78a71205d48ce22a56d54c871712e32ac9494c
ferryhinardi <ferry.hinardi@traveloka.com>
2024-07-31 13:40:37 +0700

    change redirect

--- src/components/RedirectButton.tsx
+++ src/components/RedirectButton.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default function RedirectButton() {
   const onClick = () => {
-    window.location.href = 'https://www.google.com';
+    window.location.href = 'https://www-pr24969.web.tvlk.dev/en-id/flight/affiliate/redirect?ap=CGK.SIN&dt=13-7-2024.15-7-2024&ps=1.0.0&sc=ECONOMY';
   };
   return (
     <button onClick={onClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
